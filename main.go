753da93bf1222d01559721410a657322ef0956e6
Jane He <j33he@uwaterloo.ca>
2025-04-23 23:43:55 -0400

    completed ui changes

--- main.go
+++ main.go
@@ -1,30 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
-
-var shortGolang = "Watch Go crash Course"
-var fullGolang = "Watch Nana's Golang Full Course"
-var rewardDessert = "Reward myself with a donut"
-var taskItems = []string{shortGolang, fullGolang, rewardDessert}
 
 // note := shorthand only works for local vars not global
 
 func main() {
-	http.HandleFunc("/", helloUser)
-	http.HandleFunc("/show-tasks", showTasks)
-	http.ListenAndServe(":8080", nil) // ignore if something happens
-}
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
 
-func showTasks(writer http.ResponseWriter, request *http.Request) {
-	for _, task := range taskItems {
-		fmt.Fprintln(writer, task)
+	log.Println("server started at http://localhost:8081")
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Fatal(err)
 	}
-}
-
-func helloUser(writer http.ResponseWriter, request *http.Request) {
-	var greeting = "Hello user. Welcome to out Todolist App!"
-	fmt.Fprintln(writer, greeting)
 }
